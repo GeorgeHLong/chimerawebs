d750d4fd799786ca5af37469cb1dea9e4ec186ce
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-17 12:31:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,19 +26,13 @@ with left_column:
 with right_column:
     infra = st.text_input('Infrastructure','')
 
-'Starting a long computation...'
+if "df" not in st.session_state:
+    st.session_state.df = pd.DataFrame(np.random.randn(20, 2), columns=["x", "y"])
 
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
-
-'...and now we\'re done!'
+st.header("Choose a datapoint color")
+color = st.color_picker("Color", "#FF0000")
+st.divider()
+st.scatter_chart(st.session_state.df, x="x", y="y", color=color)
 
 # Process input only if data is provided
 if data:
