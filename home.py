c5e249fb3a0b09c6d672b733020b4a263554c1ed
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-17 16:00:55 -0400

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-create_page = st.Page("pages/Home.py.py", title="Create entry", icon=":material/add_circle:")
-delete_page = st.Page("pages/Alliance_Military_Data.py.py", title="Delete entry", icon=":material/delete:")
+create_page = st.Page("pages/Home.py", title="Create entry", icon=":material/add_circle:")
+delete_page = st.Page("pages/Alliance_Military_Data.py", title="Delete entry", icon=":material/delete:")
 
 pg = st.navigation([create_page, delete_page])
 st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
