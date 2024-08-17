56a64c39da4b75dd95b89051d22ea66df602cbb6
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-17 16:06:59 -0400

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-create_page = st.Page("pages/Home.py", title="Home", icon=":material/add_circle:")
-delete_page = st.Page("pages/Alliance_Military_Data.py", title="Alliance Military Data", icon=":material/delete:")
+create_page = st.Page("pages/Home.py", title="Home", icon=":material/home:")
+delete_page = st.Page("pages/Alliance_Military_Data.py", title="Alliance Military Data", icon=":material/radar:")
 
 pg = st.navigation([create_page, delete_page])
 st.set_page_config(page_title="Chimera Corp", page_icon="/images/chimera.png")
