c899cf5845317d31e142e58ce5ac06746bf31145
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-17 12:45:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import numpy as np
 import time
 
 
-conn = st.connection("chimera")
+conn = st.connection("my_database")
 df = conn.query("select * from registeredalliances")
 st.dataframe(df)
 def run_script(infra_needed, imp_total, imp_coalpower, imp_oilpower, imp_windpower, imp_nuclearpower, imp_coalmine, imp_oilwell, imp_uramine, imp_leadmine, imp_ironmine, imp_bauxitemine, imp_farm, imp_gasrefinery, imp_aluminumrefinery, imp_munitionsfactory, imp_steelmill, imp_policestation, imp_hospital, imp_recyclingcenter, imp_subway, imp_supermarket, imp_bank, imp_mall, imp_stadium, imp_barracks, imp_factory, imp_hangars, imp_drydock):
