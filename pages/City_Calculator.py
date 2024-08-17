c45b30cea2ac19a403318070de363293501e14c0
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-17 16:48:11 -0400

    Update City_Calculator.py

--- pages/City_Calculator.py
+++ pages/City_Calculator.py
@@ -17,6 +17,9 @@ with center:
 with right_column:
     st.write("Projects")
     armstockpile = st.checkbox("Arms Stockpile")
+    bauxiteworks = st.checkbox("Bauxite Works")
+    emergencygas = st.checkbox("Emergency Gasoline Reserve")
+    
 def run_script(infra_needed, imp_total, imp_coalpower, imp_oilpower, imp_windpower, imp_nuclearpower, imp_coalmine, imp_oilwell, imp_uramine, imp_leadmine, imp_ironmine, imp_bauxitemine, imp_farm, imp_gasrefinery, imp_aluminumrefinery, imp_munitionsfactory, imp_steelmill, imp_policestation, imp_hospital, imp_recyclingcenter, imp_subway, imp_supermarket, imp_bank, imp_mall, imp_stadium, imp_barracks, imp_factory, imp_hangars, imp_drydock):
     # Replace this with your actual Python script logic
     bauxiteproduced = ((imp_bauxitemine*3)*(1+(0.5/9)*(imp_bauxitemine-1)))
