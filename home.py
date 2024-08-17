6e2e1f8cf9600419a958b1bb736b038587121e23
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-17 15:55:38 -0400

    Update home.py

--- home.py
+++ home.py
@@ -5,7 +5,6 @@ citycalc = st.Page("pages/City_Calculator.py", title="City Calculator")
 
 
 pg = st.navigation([home, citycalc])
-st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
 pg.run()
 
 st.image("images/banner.png")
