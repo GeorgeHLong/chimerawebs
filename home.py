bd9d0161ea861551d13000b33887a627b7beed62
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-17 15:57:17 -0400

    Update home.py

--- home.py
+++ home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config(page_title="Home", page_icon=":material/edit:")
 
 home = st.Page("home.py", title="Home")
 citycalc = st.Page("pages/City_Calculator.py", title="City Calculator")
