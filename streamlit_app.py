bf6598acf74929d31dfa86c8a8707c9679947e39
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-17 12:26:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,11 +24,13 @@ with left_column:
     land = st.text_input('Land','')
 with right_column:
     infra = st.text_input('Infrastructure','')
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
 
-st.dataframe(dataframe.style.highlight_max(axis=0))
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
 
 # Process input only if data is provided
 if data:
