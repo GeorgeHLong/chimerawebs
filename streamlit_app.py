1d32ae6b04be141a9455e136cd5565c7bc3fd557
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-17 12:39:36 -0400

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,13 +26,7 @@ with left_column:
 with right_column:
     infra = st.text_input('Infrastructure','')
 
-    if "df" not in st.session_state:
-        st.session_state.df = pd.DataFrame(np.random.randn(20, 2), columns=["x", "y"])
 
-    st.header("Choose a datapoint color")
-    color = st.color_picker("Color", "#FF0000")
-    st.divider()
-    st.scatter_chart(st.session_state.df, x="x", y="y", color=color)
 
 # Process input only if data is provided
 if data:
