e8229298703a669c723c6bc0e10d951c29cefd13
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-17 16:21:39 -0400

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -10,9 +10,12 @@ st.image("images/banner.png")
 conn = st.connection("postgresql", type="sql")
 # Run a query
 st.markdown("# Welcome to Chimera Corp.")
-st.image("images/homepage1.jpeg")
+st.image("images/homepage1.jpeg",)
 
 st.write("Welcome to Chimera Corp, your trusted partner in data analysis and strategic insights for the game Politics and War. At Chimera Corp, we specialize in transforming complex in-game data into actionable intelligence, empowering alliances and nations to make informed decisions. Whether you're looking to optimize your nation's performance, outmaneuver your rivals, or gain a competitive edge, our cutting-edge analytics and tailored solutions will help you conquer the political landscape. Join us and unlock the full potential of your nation with Chimera Corp—where data meets domination.")
+
+st.markdown("## Empowering Orbis with Quality Effectively")
+st.write("Chimera Corp is dedicated to revolutionizing the gaming industry through cutting-edge technology. With our state-of-the-art Discord Bots, Alliance Consultancy Services, and Data Analysis tools, we provide gamers with the tools they need to excel for whatever they need. Join us and unlock a new level of gaming expertise.")
 
 def run_script(infra_needed, imp_total, imp_coalpower, imp_oilpower, imp_windpower, imp_nuclearpower, imp_coalmine, imp_oilwell, imp_uramine, imp_leadmine, imp_ironmine, imp_bauxitemine, imp_farm, imp_gasrefinery, imp_aluminumrefinery, imp_munitionsfactory, imp_steelmill, imp_policestation, imp_hospital, imp_recyclingcenter, imp_subway, imp_supermarket, imp_bank, imp_mall, imp_stadium, imp_barracks, imp_factory, imp_hangars, imp_drydock):
     # Replace this with your actual Python script logic
