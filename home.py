488d87a1f5f479a732445a841d5b30cc76234231
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-17 15:53:04 -0400

    Update home.py

--- home.py
+++ home.py
@@ -1,9 +1,14 @@
 import streamlit as st
 
-home = st.Page("home.py", title="Home Page")
+home = st.Page("home.py", title="Home")
 citycalc = st.Page("pages/City_Calculator.py", title="City Calculator")
 
 
 pg = st.navigation([home, citycalc])
 st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
 pg.run()
+
+st.image("images/banner.png")
+conn = st.connection("postgresql", type="sql")
+# Run a query
+st.markdown("# Welcome to Chimera Corp.")
