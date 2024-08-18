dce93bf5a05c155de2a3883fbc439cb263fea5d7
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-17 21:56:25 -0400

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -11,19 +11,18 @@ conn = st.connection("postgresql", type="sql")
 # Run a query
 
 st.write("Welcome to Chimera Corp, your trusted partner in data analysis and strategic insights for the game Politics and War. At Chimera Corp, we specialize in transforming complex in-game data into actionable intelligence, empowering alliances and nations to make informed decisions. Whether you're looking to optimize your nation's performance, outmaneuver your rivals, or gain a competitive edge, our cutting-edge analytics and tailored solutions will help you conquer the political landscape. Join us and unlock the full potential of your nation with Chimera Corp—where data meets domination.")
-st.image("images/homepage1.jpeg",)
 
 st.markdown("## Cutting-Edge Data Analysis")
+st.image("images/homepage1.jpeg",)
 st.write("Unlock the power of data with AI-driven algorithms for game trends and player behavior insights")
 
-st.image("images/homepage2.jpeg",)
-
 st.markdown("## Empowering Orbis with Quality Effectively")
+st.image("images/homepage2.jpeg",)
 st.write("Chimera Corp is dedicated to revolutionizing the gaming industry through cutting-edge technology. With our state-of-the-art Discord Bots, Alliance Consultancy Services, and Data Analysis tools, we provide gamers with the tools they need to excel for whatever they need. Join us and unlock a new level of gaming expertise.")
 
-st.image("images/ai.jpeg",)
 
 st.markdown("## AI-Driven Technology")
+st.image("images/ai.jpeg",)
 st.write("Enhance your gameplay with AI-powered bots that streamline communication and coordination")
 
 
