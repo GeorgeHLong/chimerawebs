207941ccf11d343958aee520c9828c95e1be8655
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-18 17:42:04 -0400

    Update peerreport.py

--- pages/peerreport.py
+++ pages/peerreport.py
@@ -5,6 +5,8 @@ import pandas as pd
 # Display the banner image
 st.image("images/banner.png")
 
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
 # Connect to the database
 conn = st.connection("postgresql", type="sql")
 
