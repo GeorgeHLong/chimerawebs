f0161162735452d299fe25de8e4b5ae3bde1b3ea
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-18 13:39:03 -0400

    Update home.py

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ create_page = st.Page("pages/Home.py", title="Home", icon=":material/home:")
 delete_page = st.Page("pages/Alliance_Military_Data.py", title="Alliance Military Data", icon=":material/analytics:")
 citycalc = st.Page("pages/City_Calculator.py", title="City Build Calculator", icon=":material/apartment:")
 
-nationtiering = st.Page("pages/Nation_Tiering_System.py", title="Nation Tiering Data", icon=":material/data_table:")
+nationtiering = st.Page("pages/Nation_Tiering_System.py", title="Avg. City Income by Alliance over Time", icon=":material/history:")
 
 pg = st.navigation([create_page, delete_page,citycalc,nationtiering])
 st.set_page_config(page_title="Chimera Corp", page_icon="/images/chimera.png")
