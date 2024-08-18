c17555de3fde73be6c6a7857c896212dac2a8268
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-18 10:38:54 -0400

    Update City_Calculator.py

--- pages/City_Calculator.py
+++ pages/City_Calculator.py
@@ -158,8 +158,8 @@ if submit:
             result = run_script(parsed_data,infra,land,armstockpile,bauxiteworks,emergencygas,ironworks,uraniumenrich,clinicalresearch,greentech,governmentsupport,itc,massirrigation,recycling,policeprogram,telesat,openmarkets)
             with st.container():
                 food_price,coal_price,oil_price,uranium_price,bauxite_price,lead_price,gasoline_price,munitions_price,aluminum_price,steel_price,commercerev, disease, pollutionidx, bauxiteproduced, ironproduced, leadproduced, oilproduced, coalproduced, uraniumproduced, foodproduced, steelproduced, gasproduced, aluminumproduced, munitionsproduced = result
-                commerce = "$"+commercerev
-                st.markdown(commercerev)
+                commerce = ("$"+commercerev)
+                st.markdown(commerce)
         except json.JSONDecodeError as e:
             st.error(f"JSON decode error: {e}")
     else:
