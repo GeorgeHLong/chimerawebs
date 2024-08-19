2889d64cc7e28546f66bd6a75bb371915c7e1358
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-19 13:44:05 -0400

    Update peerreport.py

--- pages/peerreport.py
+++ pages/peerreport.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 # Display the banner image
 st.image("images/banner.png")
-st.write("DB username:", st.secrets["username"])
-st.write("DB password:", st.secrets["password"])
+st.write("DB username:", st.secrets["db_credentials"]["username"])
+st.write("DB password:", st.secrets["db_credentials"]["password"])
 
