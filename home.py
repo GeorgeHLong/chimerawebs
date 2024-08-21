cd26c97d5a270687032311e4f5a885f41b245c17
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-21 09:54:02 -0400

    Update home.py

--- home.py
+++ home.py
@@ -5,9 +5,9 @@ delete_page = st.Page("pages/Alliance_Military_Data.py", title="Alliance Militar
 citycalc = st.Page("pages/City_Calculator.py", title="City Build Calculator", icon=":material/apartment:")
 
 avgcityrev= st.Page("pages/Nation_Tiering_System.py", title="Avg. City Revenue by Alliance", icon=":material/attach_money:")
-alliancetiering = st.Page("pages/alliance_tiering.py", title="Alliance Tiering", icon=":material/apartment:")
+alliancetiering = st.Page("pages/alliance_tiering.py", title="Alliance Tiering", icon=":material/equalizer:")
 peerreport = st.Page("pages/peerreport.py", title="Alliance Tiering", icon=":material/summarize:")
-cityoptimizer = st.Page("pages/cityoptimizer.py", title="City Optimizer")
+cityoptimizer = st.Page("pages/cityoptimizer.py", title="City Optimizer",icon=":material/monitoring")
 
 pg = st.navigation([create_page, delete_page,citycalc,avgcityrev,peerreport,cityoptimizer])
 st.set_page_config(page_title="Chimera Corp", page_icon="./images/chimera.png")
