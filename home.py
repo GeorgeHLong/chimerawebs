15f5a83fe1ce2453c73dd23e9cc6f8d3d6a3cb7c
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-21 09:51:08 -0400

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 create_page = st.Page("pages/Home.py", title="Home", icon=":material/home:")
-delete_page = st.Page("pages/Alliance_Military_Data.py", title="Alliance Military Data", icon=":material/analytics:")
+delete_page = st.Page("pages/Alliance_Military_Data.py", title="Alliance Military Data", icon=":material/military_tech:")
 citycalc = st.Page("pages/City_Calculator.py", title="City Build Calculator", icon=":material/apartment:")
 
 avgcityrev= st.Page("pages/Nation_Tiering_System.py", title="Avg. City Revenue by Alliance", icon=":material/attach_money:")
