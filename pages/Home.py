af279cd99d9942fa524b657a7a2a309854f7548c
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-23 16:18:38 -0400

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -23,7 +23,8 @@ if not value.empty:
     nationid = value.loc[0, 'nation_id']
     username = value.loc[0, 'username']
     password = value.loc[0, 'password']
-    nationid = int(nationid)
+    nationid = list(nationid)
+    
     st.write(type(username),type(nationid),type(password))
 else:
     st.write("No data found for the given query.")
