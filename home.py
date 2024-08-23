405a969c136ef1d75cfc9cac6893432ae4f6beba
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-23 15:34:34 -0400

    Update home.py

--- home.py
+++ home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator
 
 create_page = st.Page("pages/Home.py", title="Home", icon=":material/home:")
 delete_page = st.Page("pages/Alliance_Military_Data.py", title="Alliance Military Data", icon=":material/military_tech:")
@@ -10,7 +11,34 @@ peerreport = st.Page("pages/peerreport.py", title="Alliance Tiering", icon=":mat
 cityoptimizer = st.Page("pages/cityoptimizer.py", title="City Optimizer",icon=":material/monitoring:")
 beigeturn = st.Page("pages/beige_turn.py", title="Beige Sniper",icon=":material/crisis_alert:")
 
+hide_bar= """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        visibility:hidden;
+        width: 0px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        visibility:hidden;
+    }
+    </style>
+"""
 
+names = ["Peter Parker", "Rebecca Miller","bharath"]
+usernames = ["pparker", "rmiller","bharath"]
+hashed_passwords = []
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "SIPL_dashboard", "abcdef")
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+    st.markdown(hide_bar, unsafe_allow_html=True)
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+    st.markdown(hide_bar, unsafe_allow_html=True)
+    
 pg = st.navigation([create_page, delete_page,citycalc,avgcityrev,peerreport,beigeturn])
 st.set_page_config(page_title="Chimera Corp", page_icon="./images/chimera.png")
 pg.run()
