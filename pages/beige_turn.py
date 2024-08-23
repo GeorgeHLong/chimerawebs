fbd4143e35419170331808f3e37531d1db385d3e
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-23 12:27:08 -0400

    Update beige_turn.py

--- pages/beige_turn.py
+++ pages/beige_turn.py
@@ -19,7 +19,7 @@ def ma_inf(allianceids,nationid):
     select score from tiny_nations tn where id = '{nationid}'
     """
     value = conn.query(query2)
-    st.write(value)
+    st.write(value['score'])
     # Define SQL query
     query = f"""
     select id, num_cities, score, beige_turns from tiny_nations tn where alliance_id in ({allianceids}) and beige_turns > 0 and tax_id != 0 order by num_cities 
