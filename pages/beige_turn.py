e999dd9053999c35026e5fa4a36bdd0a4ee43a82
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-23 12:26:07 -0400

    Update beige_turn.py

--- pages/beige_turn.py
+++ pages/beige_turn.py
@@ -14,7 +14,13 @@ st.write("See how many turns people have left")
 
 conn = st.connection("postgresql", type="sql")
 
-def ma_inf(allianceids):
+def ma_inf(allianceids,nationid):
+    query2 = f"""
+    select score from tiny_nations tn where id = '{nationid}'
+    """
+    value = conn.query(query2)
+    value = value[0]
+    st.write(value)
     # Define SQL query
     query = f"""
     select id, num_cities, score, beige_turns from tiny_nations tn where alliance_id in ({allianceids}) and beige_turns > 0 and tax_id != 0 order by num_cities 
@@ -31,10 +37,11 @@ def ma_inf(allianceids):
 # Create the form for user input
 with st.form("my_form"):
     allianceids = st.text_input("Alliance IDs")
+    nationid = st.text_input("Nation ID")
     submit = st.form_submit_button("Submit")
 
 if submit:
-    df = ma_inf(allianceids)
+    df = ma_inf(allianceids,nationid)
     
     # Display the DataFrame with hyperlinks
     st.write(df[['Nation Link', 'num_cities', 'score', 'beige_turns']].to_markdown(index=False))
