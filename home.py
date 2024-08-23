2f372969555782723de396bc1d74ab3cec11f016
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-23 12:11:01 -0400

    Update home.py

---
+++ home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+create_page = st.Page("pages/Home.py", title="Home", icon=":material/home:")
+delete_page = st.Page("pages/Alliance_Military_Data.py", title="Alliance Military Data", icon=":material/military_tech:")
+citycalc = st.Page("pages/City_Calculator.py", title="City Build Calculator", icon=":material/apartment:")
+
+avgcityrev= st.Page("pages/Nation_Tiering_System.py", title="Avg. City Revenue by Alliance", icon=":material/attach_money:")
+alliancetiering = st.Page("pages/alliance_tiering.py", title="Alliance Tiering", icon=":material/equalizer:")
+peerreport = st.Page("pages/peerreport.py", title="Alliance Tiering", icon=":material/summarize:")
+cityoptimizer = st.Page("pages/cityoptimizer.py", title="City Optimizer",icon=":material/monitoring:")
+beigeturn = st.Page("pages/beige_turn.py", title="Beige Turn Check",icon=":material/monitoring:")
+
+
+pg = st.navigation([create_page, delete_page,citycalc,avgcityrev,peerreport,beigeturn])
+st.set_page_config(page_title="Chimera Corp", page_icon="./images/chimera.png")
+pg.run()
