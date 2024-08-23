611f9aa1fe16a817a5e993668e4d4d18d186b80c
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-23 16:20:22 -0400

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -23,7 +23,7 @@ if not value.empty:
     nationid = value.loc[0, 'nation_id']
     username = value.loc[0, 'username']
     password = value.loc[0, 'password']
-    nationid = int(nationid)
+    nationid = str(nationid)
     info = zip(nationid, username, password)
     
     st.write(info)
