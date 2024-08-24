0b03f4d7eb20535fd7fbbc8a08a403f7631b5198
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-24 00:30:45 -0400

    Update profile.py

--- pages/profile.py
+++ pages/profile.py
@@ -14,7 +14,7 @@ select Food,Coal,Oil,Uranium,iron,Bauxite,Lead,Gasoline,Munitions,Aluminum,Steel
 df0 = conn.query(query0)
 st.write(df0)
 
-left_column, right_column = st.columns(2)
+left_column, center,right_column = st.columns(3)
 with left_column:
 # Execute query and fetch results into DataFrame
     query = f"""
@@ -23,7 +23,7 @@ with left_column:
     df = conn.query(query)
     st.markdown("## Chimera Holdings")
     st.write(df.transpose())
-with right_column:
+with center:
     query3 = f"""
     select round(avg(soldiers),0) as "soldiers", round(avg(tanks),0) as "tanks", round(avg(aircraft),0) as "aircraft", round(avg(ships),0) as "ships", round(avg(missiles),0) as "missiles", round(avg(nukes),0) as "nukes", round(avg(spies),0) as "spies" from tiny_nations tn where score between 8000 and 9000
     """
@@ -36,5 +36,10 @@ with right_column:
     """
     df2 = conn.query(query2)
     st.markdown("## Military Info")    
-    st.write(df2.transpose())    
+    st.write(df2.transpose())
+with right_column:
+    combined_df = pd.concat([df2, df3], axis=1)
+    fig = px.bar(combined_df, x='Category', y=['Value1', 'Value2'], barmode='group')
+    st.bar_chart(combined_df,x="Military Info", y="Amount", stack=False)
+
     
