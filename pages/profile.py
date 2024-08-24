c80b72e227cece8264f8eec83ba06f0e5a85ed03
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-24 00:10:57 -0400

    Update profile.py

--- pages/profile.py
+++ pages/profile.py
@@ -7,11 +7,15 @@ st.image("images/banner.png")
 conn = st.connection("postgresql", type="sql")
 nationid = int(st.session_state.role)
 
-query = f"""
-select money,food,coal,oil,uranium,lead,iron,bauxite,gasoline,munitions,steel,aluminum from bankaccounts where nation_id = '{nationid}'
-"""
 
+left_column, center,right_column = st.columns(3)
+with left_column:
 # Execute query and fetch results into DataFrame
-df = conn.query(query)
-st.markdown("## Bank Account Holdings")
-st.write(df.transpose())
+    query = f"""
+    select money,food,coal,oil,uranium,lead,iron,bauxite,gasoline,munitions,steel,aluminum from bankaccounts where nation_id = '{nationid}'
+    """
+    df = conn.query(query)
+    st.markdown("## Bank Account Holdings")
+    st.write(df.transpose())
+with center:
+    
