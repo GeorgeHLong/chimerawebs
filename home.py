2ce618f8483632f1b34b81c7aaeacb2bc89180a7
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-23 23:46:15 -0400

    Update home.py

--- home.py
+++ home.py
@@ -38,7 +38,7 @@ def login():
 
 def logout():
     st.image("images/banner.png")
-    st.markdown("# Log out of Chimera Web")
+    st.markdown("## Log out of Chimera Web")
     if st.button("Log out"):
         st.session_state.logged_in = False
         st.rerun()
