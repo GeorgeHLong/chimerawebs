e0a865373b531148d509bbfa98adea346732ea8d
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-23 22:38:37 -0400

    Update home.py

--- home.py
+++ home.py
@@ -1,4 +1,19 @@
 import streamlit as st
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+def login():
+    if st.button("Log in"):
+        st.session_state.logged_in = True
+        st.rerun()
+
+def logout():
+    if st.button("Log out"):
+        st.session_state.logged_in = False
+        st.rerun()
+
+login_page = st.Page(login, title="Log in", icon=":material/login:")
+logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 
 create_page = st.Page("pages/Home.py", title="Home", icon=":material/home:")
 delete_page = st.Page("pages/Alliance_Military_Data.py", title="Alliance Military Data", icon=":material/military_tech:")
@@ -10,7 +25,10 @@ peerreport = st.Page("pages/peerreport.py", title="Alliance Tiering", icon=":mat
 cityoptimizer = st.Page("pages/cityoptimizer.py", title="City Optimizer",icon=":material/monitoring:")
 beigeturn = st.Page("pages/beige_turn.py", title="Beige Sniper",icon=":material/crisis_alert:")
 
-
-pg = st.navigation([create_page, delete_page,citycalc,avgcityrev,peerreport,beigeturn])
+if st.session_state.logged_in:
+    pg = st.navigation([create_page, delete_page,citycalc,avgcityrev,peerreport,beigeturn])
+else:
+    pg = st.navigation([login_page])
+    
 st.set_page_config(page_title="Chimera Corp", page_icon="./images/chimera.png")
 pg.run()
