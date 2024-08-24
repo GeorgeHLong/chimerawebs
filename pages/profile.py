0c024322567e6034dd364d9cfd790cd9a2083bd7
ncbartel <81101659+GeorgeHLong@users.noreply.github.com>
2024-08-24 00:16:58 -0400

    Update profile.py

--- pages/profile.py
+++ pages/profile.py
@@ -7,6 +7,11 @@ st.image("images/banner.png")
 conn = st.connection("postgresql", type="sql")
 nationid = int(st.session_state.role)
 
+query0 = f"""
+select Food,Coal,Oil,Uranium,iron,Bauxite,Lead,Gasoline,Munitions,Aluminum,Steel from tradeprices t order by trade_timestamp desc limit 1
+"""
+df0 = conn.query(query0)
+st.write(df0)
 
 left_column, right_column = st.columns(2)
 with left_column:
